f83c849d3c01867a0d5d80f5fc9c3f624c0f41ff
unknown <engineeringc122@gmail.com>
2021-05-24 21:10:59 +0300

    TASK 4 DONE

--- calculator.js
+++ calculator.js
@@ -1,6 +1,12 @@
-const prompt = require("prompt-sync")(); // Please don't touch me :)
-
-// The prompt method allows us to take an input from the user.
-// Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+let firstNmber = 5;
+let secondNmber =7;
+let operation ="*";
+if( operation==="+"){
+    console.log(`${firstNmber}+${secondNmber}=${firstNmber+secondNmber}`)
+}else if (operation==="-"){ 
+   console.log(`${firstNmber}-${secondNmber}=${firstNmber-secondNmber}`)  
+}else if (operation==="*"){
+     console.log(`${firstNmber}*${secondNmber}=${firstNmber*secondNmber}`)
+}else if (operation==="/"){
+ console.log(`${firstNmber}/${secondNmber}=${firstNmber/secondNmber}`)
+}else console.log("Error operation")
